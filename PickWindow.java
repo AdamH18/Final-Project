ea1ba024b7377a0cdc5dcecae413db4192a616b8
AdamH18 <adamh181818@gmail.com>
2016-05-09 12:19:02 -0400

    Basic Stuff

---
+++ PickWindow.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class PickWindow{
+  
+  
+  public PickWindow(){
+    JFrame pkPick = new JFrame();
+    pkPick.setTitle("Pick a Pokemon to use");
+    pkPick.setSize(270, 500);
+    pkPick.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    pkPick.setVisible(true);
+  }
+  
+  
+}
