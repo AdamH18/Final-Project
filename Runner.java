ea8ab8e37a264c19cc5b491e16f856e4f0f86e86
AdamH18 <adamh181818@gmail.com>
2016-05-10 12:34:39 -0400

    New

--- Runner.java
+++ Runner.java
@@ -1,5 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.charset.*;
+
 public class Runner{
   public static void main(String[]args){
-    PickWindow wind = new PickWindow();
+    Path dataPath = Paths.get("Data", "Pokemon.txt");
+    try{
+      List<String> pokemon = Files.readAllLines(dataPath, StandardCharsets.US_ASCII);
+      pokemon.remove(0);
+      PickWindow wind = new PickWindow(pokemon);
+    } catch(IOException e){
+      System.out.println(e);
+    }
   }
 }
